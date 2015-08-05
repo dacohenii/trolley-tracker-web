dda28b8676cdc5b739090f63b17c1cd973957c8f
John Mark Smotherman <linktheoriginal@gmail.com>
2015-08-05 00:27:08 -0400

    Running from New API.  Pulling all Trolley data.

--- trolley/trolleytracker/views.py
+++ trolley/trolleytracker/views.py
@@ -4,8 +4,11 @@ import urllib.request
 
 # Create your views here.
 def track(request):
-	return render(request, 'trolleytracker/index.html')
+	context = {
+		'datarequest': urllib.request.urlopen(urllib.request.Request("http://yeahthattrolley.azurewebsites.net/api/v1/Stops")).read()
+	}
+	return render(request, 'trolleytracker/index.html', context)
 	
 def update(request):
-	datarequest = urllib.request.Request("http://104.131.44.166/api/v1/trolly/" + request.GET['id'].strip() + "/location")
+	datarequest = urllib.request.Request("http://yeahthattrolley.azurewebsites.net/api/v1/Trolleys")
 	return HttpResponse(urllib.request.urlopen(datarequest).read())

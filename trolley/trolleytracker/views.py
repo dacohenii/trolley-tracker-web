a22256c9e69cb889194d6dc60384fa0055a15876
John Mark Smotherman <linktheoriginal@gmail.com>
2015-08-18 22:37:09 -0400

    Pulling route data from api, multi routes, info popup.

--- trolley/trolleytracker/views.py
+++ trolley/trolleytracker/views.py
@@ -1,14 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import urllib.request
+import json
 
 # Create your views here.
 def track(request):
+	#get the list of currently active routes
+	activeroutes = json.loads(urllib.request.urlopen(urllib.request.Request("http://tracker.wallinginfosystems.com/api/v1/Routes/Active")).read().decode('utf-8'))
+	routes = []
+
+	for route in activeroutes:
+		#get the route definition for each active route
+		routes.append(json.loads(urllib.request.urlopen(urllib.request.Request("http://tracker.wallinginfosystems.com/api/v1/Routes/" + str(route['ID']))).read().decode('utf-8')))
+
+	#get the active trolley data
+	trolleys = json.loads(urllib.request.urlopen(urllib.request.Request("http://tracker.wallinginfosystems.com/api/v1/Trolleys")).read().decode('utf-8'))
+
 	context = {
-		'datarequest': urllib.request.urlopen(urllib.request.Request("http://yeahthattrolley.azurewebsites.net/api/v1/Stops")).read()
+		'routes': json.dumps(routes)
 	}
 	return render(request, 'trolleytracker/index.html', context)
 	
 def update(request):
-	datarequest = urllib.request.Request("http://yeahthattrolley.azurewebsites.net/api/v1/Trolleys")
+	datarequest = urllib.request.Request("http://tracker.wallinginfosystems.com/api/v1/Trolleys")
 	return HttpResponse(urllib.request.urlopen(datarequest).read())
